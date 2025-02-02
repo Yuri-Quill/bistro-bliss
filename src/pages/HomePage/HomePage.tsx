7c2f734a4aa2f8265cbaf1cb966f3321382f7ed3
Yuri Quill <yury.fortis@gmail.com>
2025-02-02 17:46:33 +0200

    Added BrowseMenuSection component to display menu categories Implemented BrowseMenuCard for individual menu category Styled components using BEM methodology Optimized image loading with loading="lazy"

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,12 @@
+import BrowseMenuSection from "../../Components/BrowseMenuSection/BrowseMenuSection";
 import HeroSection from "../../Components/HeroSection/HeroSection";
 
 const HomePage = () => {
 	return (
-		<HeroSection/>
+		<>
+			<HeroSection />
+			<BrowseMenuSection />
+		</>
 	);
 };
 
