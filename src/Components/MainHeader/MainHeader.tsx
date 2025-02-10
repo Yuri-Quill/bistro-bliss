ce3f42e98769a3b5bd7111578fe9dcc1bfb21008
Yuri Quill <yury.fortis@gmail.com>
2025-02-10 19:28:17 +0200

    added buttons component

--- src/Components/MainHeader/MainHeader.tsx
+++ src/Components/MainHeader/MainHeader.tsx
@@ -1,11 +1,53 @@
+import { Link } from "react-router-dom";
+
+import Container from "../Container/Container";
 import TopBar from "../TopBar/TopBar";
+import { ButtonLink } from "../Buttons/Buttons";
+
+import HeaderLogo from "../../assets/bistro-bliss-logo.svg";
+
+import { mainMenu } from "../../shared/data/main-menu.data";
+
 import "./MainHeader.scss";
 
 const MainHeader = () => {
 	return (
 		<>
 			<TopBar />
-			<header className="header">header</header>
+			<header className="header">
+				<Container>
+					<Link className="header-logo" to="/">
+						<img
+							className="header-logo__image"
+							src={HeaderLogo}
+							alt="Bistro Bliss logo image"
+							width={57}
+							height={55}
+						/>
+						<h2 className="header-logo__title">Bistro Bliss</h2>
+					</Link>
+
+					<nav className="header-nav">
+						<ul className="header-list">
+							{mainMenu.map((item, index) => (
+								<li className="header-item" key={index}>
+									<Link
+										className="header-link"
+										to={item.url}
+										aria-label={item.description}
+									>
+										{item.name}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</nav>
+
+					<ButtonLink to="/" aria-label="Book A Table" className="header-button">
+						Book A Table
+					</ButtonLink>
+				</Container>
+			</header>
 		</>
 	);
 };
---
+++ src/shared/data/main-menu.data.tsx
@@ -0,0 +1,29 @@
+import { IMainMenu } from "../interfaces/MainMenu.interface";
+
+export const mainMenu: IMainMenu[] = [
+	{
+		name: "Home",
+		url: "/",
+		description: "Welcome to Bistro Bliss",
+	},
+	{
+		name: "About",
+		url: "/about",
+		description: "Learn more about our restaurant",
+	},
+	{
+		name: "Menu",
+		url: "/menu",
+		description: "Browse our delicious menu items",
+	},
+	{
+		name: "Pages",
+		url: "/pages",
+		description: "Explore our website",
+	},
+	{
+		name: "Contact",
+		url: "/contact",
+		description: "Get in touch with us",
+	},
+];
