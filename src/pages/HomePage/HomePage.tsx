7b2a61f81978bbf74922b6621638921db4f21ab0
Yuri Quill <yury.fortis@gmail.com>
2025-02-12 21:35:10 +0200

    add delivery section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import FeaturedMenu from "../../Components/FeaturedMenu/FeaturedMenu";
 import Hero from "../../Components/Hero/Hero";
 import MeetUs from "../../Components/MeetUs/MeetUs";
+import OurDelivery from "../../Components/OurDelivery/OurDelivery";
 import OurServices from "../../Components/OurServices/OurServices";
 
 import MeetUsKebab from '../../assets/meet-us-section/meet-us-kebab.png'
@@ -12,6 +13,7 @@ const HomePage = () => {
 			<FeaturedMenu />
 			<MeetUs image={MeetUsKebab}/>
 			<OurServices/>
+			<OurDelivery/>
 		</>
 	);
 };
