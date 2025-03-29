ed8977e9b0435b74de8d398309f7003598d442d2
Yuri Quill <yury.fortis@gmail.com>
2025-03-29 02:22:51 +0200

    added some fixes for detail page menu

--- src/pages/MenuDetailPage/MenuDetailPage.tsx
+++ src/pages/MenuDetailPage/MenuDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { useEffect } from "react";
 
@@ -9,23 +9,42 @@ import Loading from "../../Components/Loading/Loading";
 import NoImage from "../../assets/no-img.jpg";
 import "./MenuDetailPage.scss";
 import cn from "classnames";
+import ButtonLink from "../../Components/ButtonLink/ButtonLink";
 
 const MenuDetailPage = () => {
 	const dispatch = useAppDispatch();
-	const { loading, selectedItem } = useAppSelector((state) => state.menu);
+	const navigate = useNavigate();
+	const { loading, selectedItem, categories } = useAppSelector(
+		(state) => state.menu
+	);
 
 	const { categories: categoryParam, id } = useParams<{
 		categories: string;
 		id: string;
 	}>();
 
-	useEffect(() => {
-		if (id && categoryParam) {
-			dispatch(getMenuItemById({ category: categoryParam, id: id }));
-		}
-	}, [dispatch, categoryParam, id]);
+	let trueCategory: string | undefined;
 
-	const detailAlcoholCn = cn("menu-detail__subtext","menu-detail__alcohol", {
+	if (categoryParam === "all") {
+		trueCategory = categories.find((cat) =>
+			cat.items.some((item) => item._id === id)
+		)?.category;
+
+		if (trueCategory) {
+			navigate(`/menu/${trueCategory}/${id}`, { replace: true });
+		}
+	} else {
+		trueCategory = categoryParam;
+	}
+
+	console.log(trueCategory);
+	useEffect(() => {
+		if (id && trueCategory) {
+			dispatch(getMenuItemById({ category: trueCategory, id }));
+		}
+	}, [dispatch, id, trueCategory]);
+
+	const detailAlcoholCn = cn("menu-detail__subtext", "menu-detail__alcohol", {
 		"menu-detail__alcohol--active": selectedItem?.alcohol_content,
 	});
 
@@ -37,7 +56,7 @@ const MenuDetailPage = () => {
 				) : (
 					selectedItem && (
 						<>
-                        <h2 className="menu-detail__title">{selectedItem.name}</h2>
+							<h2 className="menu-detail__title">{selectedItem.name}</h2>
 							<figure className="menu-detail__figure">
 								<img
 									className="menu-detail__image"
@@ -48,7 +67,6 @@ const MenuDetailPage = () => {
 									loading="lazy"
 								/>
 								<figcaption className="menu-detail__caption">
-									
 									<h4 className="menu-detail__subtitle">
 										Price:{" "}
 										<span className="menu-detail__subtext menu-detail__subtext--price">{`$ ${selectedItem.price}`}</span>
@@ -69,7 +87,9 @@ const MenuDetailPage = () => {
 									</div>
 									<h4 className="menu-detail__subtitle">
 										Calories:{" "}
-										<span className="menu-detail__subtext menu-detail__subtext--calories">{selectedItem.calories}</span>
+										<span className="menu-detail__subtext menu-detail__subtext--calories">
+											{selectedItem.calories}
+										</span>
 									</h4>
 
 									<h4 className="menu-detail__subtitle">
@@ -92,6 +112,14 @@ const MenuDetailPage = () => {
 						</>
 					)
 				)}
+				<ButtonLink
+					className="menu-detail__link"
+					href={`/menu?category=${categoryParam}&page=1`}
+					isActive={true}
+					aria="Go back to menu page"
+				>
+					Back to menu
+				</ButtonLink>
 			</Container>
 		</section>
 	);
